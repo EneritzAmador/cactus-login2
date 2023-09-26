9b738a25bb6cacdfc1ac5c07baf602af918f6afd
Ene AL <kaixo.eneal@gmail.com>
2023-09-26 15:59:03 +0200

    arreglos

--- app.py
+++ app.py
@@ -14,8 +14,9 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 bc = Bcrypt(app)
 
-
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
